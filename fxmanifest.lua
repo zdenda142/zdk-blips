d0f58ddc15008229f37271ab52f37d8f98809cc9
zdenda142 <77118689+zdenda142@users.noreply.github.com>
2024-08-06 01:35:44 +0200

    zdk-blips

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ZdenDako'
+description 'Script pro vytváření blipů na mapě ve FiveM s použitím vector3'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'main.lua'
+}
+lua54 'yes'
---
+++ main.lua
@@ -0,0 +1,37 @@
+Config = Config or {}
+
+function CreateBlips()
+    for _, info in pairs(Config.Blips) do
+        local blip = AddBlipForCoord(info.coords)
+        SetBlipSprite(blip, info.id)
+        SetBlipDisplay(blip, info.display or 4)
+        SetBlipScale(blip, info.scale or 1.0)
+        SetBlipColour(blip, info.colour)
+        SetBlipAsShortRange(blip, info.shortRange or true)
+        SetBlipAlpha(blip, info.alpha or 255)
+
+        if info.category then
+            SetBlipCategory(blip, info.category)
+        end
+
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(info.title)
+        EndTextCommandSetBlipName(blip)
+    end
+end
+
+Citizen.CreateThread(function()
+    CreateBlips()
+end)
+
+AddEventHandler('onResourceStart', function(resource)
+    if resource == GetCurrentResourceName() then
+        print("blips is ready")
+    end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        print("blips is stopped")
+    end
+end)
